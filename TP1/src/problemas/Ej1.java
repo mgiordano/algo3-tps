4584691214726f9e53fc1db91ea6253186ab88bc
mgiordano <mgiordano@dc.uba.ar>
2012-08-24 17:15:56 -0300

    modularizado ej1

--- TP1/src/problemas/Ej1.java
+++ TP1/src/problemas/Ej1.java
@@ -1,48 +1,73 @@
 package src.problemas;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.StringTokenizer;
 
 public class Ej1 {
 	
-	public static void abrir(){
-		/*		StringTokenizer st = new StringTokenizer("this is a test");
-				while (st.hasMoreTokens()) {
-					System.out.println(st.nextToken());
-				}*/
-			//	System.out.println("temporal saraseitor is temporal\n");
-				try{
-				BufferedReader reader  = new BufferedReader( new FileReader( "/users/Juanma/code/TP1Algo3/tp1/Tp1Ej1.in" ) );
-				String linea;
-				
-				while ((linea = reader.readLine()) != null){
-					// Print the content on the console
-//					System.out.println (linea);
-					leer(linea);
-					
-				}
-				//Close the input stream
-				reader.close();
-				}catch (Exception e){//Catch exception if any
-					System.err.println("Error: " + e.getMessage());
-				}
-			}
+	public static void resolver(){
+		
+		try{
+		BufferedReader reader = new BufferedReader( new FileReader( "./src/Tp1Ej1.in" ) );
+		String linea;
+		
+		BufferedWriter os = new BufferedWriter( new FileWriter( "Tp1Ej1.out" ) );
+		while ((linea = reader.readLine()) != null){
+			escribir( buscarGanancia( leer(linea) ), os);
+		}
+		os.close();
+		reader.close();
+		
+		}catch (Exception e){
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
 			
-			private static int[] leer( String string ) {
-				   StringTokenizer st = new StringTokenizer( string, " " );
-				   int tam = st.countTokens();
-				   int[] precios = new int[tam];
-				   int pos = 0;
-				   while (st.hasMoreTokens()){
-					   precios[pos] = Integer.parseInt(st.nextToken().trim());
-					   pos++;
-				   }
-				   return precios;
-			}
+	private static int[] leer( String string ) {
+		
+	   StringTokenizer st = new StringTokenizer( string, " " );
+	   int tam = st.countTokens();
+	   int[] precios = new int[tam];
+	   int pos = 0;
+	   while (st.hasMoreTokens()){
+		   precios[pos] = Integer.parseInt(st.nextToken().trim());
+		   pos++;
+	   }
+	   return precios;
+	}
 			
-			private static void resolver(int[] precios){
+	private static int buscarGanancia(int[] precios){
+		int min = precios[0], max = precios[0], tam = precios.length;
+		int gananciaActual = 0;
+		int gananciaMax = 0;
+		for (int i = 1; i < tam; i++){
+			if (precios[i] < min){
+				min = precios[i];
+				max = precios[i];
 				
 			}
+			if (precios[i] > max){
+				max = precios[i];
+			}
+			gananciaActual = max - min;
+			if (gananciaActual > gananciaMax) gananciaMax = gananciaActual;
+		}
+		return gananciaMax;
+	}
+			
+	private static void escribir(int res, BufferedWriter os){
+		try{
+			Integer resInt = res;
+			os.append(resInt.toString());
+			System.out.println(res);
+			os.append('\n');
+			
+		}catch (Exception e){
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
 
 }
--- TP1/src/tp1.java
+++ TP1/src/tp1.java
@@ -1,8 +1,12 @@
-import src.problemas;
+package src;
+
+import src.problemas.Ej1;
 
 public class tp1{
 	
 	public static void main(String[] args){
+		
+		Ej1.resolver();
 		
 		/*Acà habrìa que llamar a los distintos ejercicios
 		 * Los ejercicios van en el paquete problemas, con sus respectivos modulos?
