7c83ffa71a8e47b167656e9ea445d63f6c30e0e8
mgiordano <mgiordano@dc.uba.ar>
2012-09-18 22:16:21 -0300

    fix investigadores

--- TP1/src/problemas/Investigador.java
+++ TP1/src/problemas/Investigador.java
@@ -10,13 +10,19 @@ public class Investigador {
 	public List<Investigador> amistades;
 	public int distancia;
 
+	public Investigador(){
+		estado = "no encontrado";
+		amistades = new ArrayList<Investigador>();
+		distancia = Integer.MAX_VALUE;
+	}
+	
 	public Investigador(String name){
 		nombre = name;
 		estado = "no encontrado";
 		amistades = new ArrayList<Investigador>();
 		distancia = Integer.MAX_VALUE;
 	}
-	
+
 	public void encontrado(){
 		estado = "encontrado";
 	}
@@ -38,13 +44,21 @@ public class Investigador {
 		boolean esta = false;
 		ListIterator<Investigador> amigos = amigo.amistades.listIterator();
 		while (amigos.hasNext()){
-			if (amigos.next().nombre.equals(amigo.nombre)){
+			if (amigos.next().nombre.equals(this.nombre)){
 				esta = true;
 			}
 		}
 		if (!esta){
 			amistades.add(amigo);
 			amigo.amistades.add(this);
+		}
+	}
+	
+	public void dameAmigos(){
+		ListIterator<Investigador> amigosIt = amistades.listIterator();
+		System.out.println("Los amigos de " + nombre + " son:");
+		while(amigosIt.hasNext()){
+			System.out.println(amigosIt.next().nombre + " ");
 		}
 	}
 }
