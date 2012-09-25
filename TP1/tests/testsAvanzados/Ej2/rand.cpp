7c9667f2708bedfbcc076552081ff0c548bf73be
mgiordano <mgiordano@dc.uba.ar>
2012-09-24 23:00:14 -0300

    bla

--- TP1/tests/testsAvanzados/Ej2/rand.cpp
+++ TP1/tests/testsAvanzados/Ej2/rand.cpp
@@ -16,21 +16,11 @@ int main(){
 	}while (random1 == random2);
 	cout << random2 << endl;
 	for(int i = 1; i <= cantNodos; i++){
-		//random1 = rand()%cantNodos + 1;
 		cout << i << " ";
 		do{
 			random2 = rand()%cantNodos + 1;
 		}while (i == random2);
 		cout << random2 << ";";
 	}
-/*	int cantNodosCuad = cantNodos*cantNodos;
-	for(int i = 0; i < cantNodosCuad; i++){
-		random1 = rand()%cantNodos + 1;
-		cout << random1 << " ";
-		do{
-			random2 = rand()%cantNodos + 1;
-		}while (random1 == random2);
-		cout << random2 << ";";
-	}*/
 	cout << endl;
 }
